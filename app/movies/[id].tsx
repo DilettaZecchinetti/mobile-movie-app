28f2a88aff126308f268c69afbdfe1055cc8905a
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-03-14 11:50:53 +0000

    display lastest movies with posters and titles

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams;
+
   return (
     <View>
-      <Text>MovieDetails</Text>
+      <Text>Movie details: {id}
+      </Text>
     </View>
   )
 }
 
 export default MovieDetails;
+
