d2477302ee48f98068bd3e111e26444ae4beaced
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-03-19 16:38:23 +0000

    trending movies section add

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,45 +1,34 @@
-import { Link } from "expo-router";
-// import MaskedView from "@react-native-masked-view/masked-view";
-import { View, Text, TouchableOpacity, Image } from "react-native";
-
 import { images } from "@/constants/images";
+import { Link } from "expo-router";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
 
-const TrendingCard = ({
-  movie: { movie_id, title, poster_url },
-  index,
-}: TrendingCardProps) => {
+const TrendingCard = ({ movie: {movie_id, title, poster_url}, index }: TrendingCardProps) => {
   return (
-    <Link href={`/movie/${movie_id}`} asChild>
-      <TouchableOpacity className="w-32 relative pl-5">
-        <Image
-          source={{ uri: poster_url }}
+    
+      <Link href={`/movies/${movie_id}`} asChild>
+        <TouchableOpacity className="w-32 relative pl-5">
+          <Image
+          source={{ uri: poster_url}}
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
-        />
 
-        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-          {/* <MaskedView
-            maskElement={
+          />
+
+          <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+            <MaskedView maskElement={
               <Text className="font-bold text-white text-6xl">{index + 1}</Text>
-            }
-          >
-            <Image
-              source={images.rankingGradient}
-              className="size-14"
-              resizeMode="cover"
-            />
-          </MaskedView> */}
-        </View>
+            }>
+              <Image source={images.rankingGradient} className="size-14" resizeMode="cover"/>
 
-        <Text
-          className="text-sm font-bold mt-2 text-light-200"
-          numberOfLines={2}
-        >
-          {title}
-        </Text>
-      </TouchableOpacity>
+            </MaskedView>
+
+          </View>
+          <Text className="text-sm font-bold mt-2 text-light-200" numberOfLines={2}>{title}</Text>
+        </TouchableOpacity>
     </Link>
-  );
-};
+  )
+}
+ 
 
 export default TrendingCard;
